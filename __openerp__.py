fc3cb7421bcca0789ddd7990a90472dc9687d9fe
jorge-torres <jorgetorresconsultor@gmail.com>
2016-06-14 02:20:27 +0200

    Prueva de primer comit, (Ventas) Crear nuevo modulo: l10n_co_tax_extension

---
+++ __openerp__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+
+{
+    'name': 'Colombia - Impuestos',
+    'category': 'Localization',
+    'version': '0.1 Beta',
+    'author': 'Jorge Torres V., Dominic Krimmer, Plastinorte S.A.S',
+    'license': 'AGPL-3',
+    'maintainer': 'dominic.krimmer@gmail.com',
+    'website': 'https://www.plastinorte.com',
+    'summary': 'Colombia Impuestos: Extended Partner / Contact Module - Odoo 9.0',
+    'images': ['images/'],
+    'description': """
+Colombia Impuestos:
+======================
+
+    * Is a module to calculate Colombian taxes.
+
+
+    """,
+    'depends': [
+
+    ],
+    'data': [
+
+    ],
+    'installable': True,
+}
+
+
