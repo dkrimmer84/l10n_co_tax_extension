cc1dc6b21a04ecec22dec79ba8b1510bbb8a380c
jorge-torres <jorgetorresconsultor@gmail.com>
2016-07-01 02:34:15 +0200

    solucionado potencial hack

--- models/l10n_co_tax_extension.py
+++ models/l10n_co_tax_extension.py
@@ -17,8 +17,17 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.       #
 ###############################################################################
 
-# Extended Partner Module
-from openerp import models, fields, api, exceptions
+from openerp import api, fields, models, _
+from openerp.tools import float_is_zero, float_compare
+from openerp.tools.misc import formatLang
+
+from openerp.exceptions import UserError, RedirectWarning, ValidationError
+
+import openerp.addons.decimal_precision as dp
+
+
+
+
 
 class ColombianTaxes(models.Model):
 
@@ -29,11 +38,34 @@ class ColombianTaxes(models.Model):
 
 # Define rfuente as new tax.
 
-    rfuente = fields.Monetary('Retencion en la fuente:', readonly="True")
+    rfuente = fields.Monetary('Retencion en la fuente:',store="True" , compute = "_compute_amount")
 
 # Calculate rfuente and total amount
 
-    @api.onchange('amount_untaxed', 'amount_total')
-    def test(self):
-        self.rfuente = self.amount_untaxed * 0.025
+
+    @api.one
+    @api.depends('invoice_line_ids.price_subtotal', 'tax_line_ids.amount', 'currency_id', 'company_id')
+    def _compute_amount(self):
+        self.amount_untaxed = sum(line.price_subtotal for line in self.invoice_line_ids)
+        self.rfuente = self.amount_untaxed * (-0.025)
+        self.amount_tax = sum(line.amount for line in self.tax_line_ids)
+        self.amount_total = self.amount_untaxed + self.amount_tax
+        amount_total_company_signed = self.amount_total
+        amount_untaxed_signed = self.amount_untaxed
+        if self.currency_id and self.currency_id != self.company_id.currency_id:
+            amount_total_company_signed = self.currency_id.compute(self.amount_total, self.company_id.currency_id)
+            amount_untaxed_signed = self.currency_id.compute(self.amount_untaxed, self.company_id.currency_id)
+        sign = self.type in ['in_refund', 'out_refund'] and -1 or 1
+        self.amount_total_company_signed = amount_total_company_signed * sign
+        self.amount_total_signed = self.amount_total * sign
+        self.amount_untaxed_signed = amount_untaxed_signed * sign
         self.amount_total = self.amount_total + self.rfuente
+
+
+
+
+
+
+
+
+
