ef6a9c35a05b682f6692c9aea3d0cd0c83379b60
Dominic Krimmer <dominic.krimmer@gmail.com>
2016-07-02 11:22:05 -0500

    fixed: much better way on how to do the calculation

--- models/l10n_co_tax_extension.py
+++ models/l10n_co_tax_extension.py
@@ -34,38 +34,17 @@ class ColombianTaxes(models.Model):
     wh_taxes = fields.Monetary('Withholding Tax:', store="True",
                                compute="_compute_amount")
 
-    # Calculate withholding tax and total amount
-    @api.depends('invoice_line_ids.price_subtotal', 'tax_line_ids.amount',
-                 'currency_id', 'company_id')
+    # Calculate withholding tax and (new) total amount
     def _compute_amount(self):
         """
         This functions computes the withholding tax on the untaxed amount
         @return: void
         """
-        self.amount_untaxed = sum(line.price_subtotal for line in
-                                  self.invoice_line_ids)
+        # Calling the original calculation
+        super(ColombianTaxes, self)._compute_amount()
 
-        # TODO: 0.025 (TWH)is a static value right now. This will be dynamic
-        # calculate colombian withholding tax
+        # Extending the calculation with the colombian withholding tax
+        # TODO: 0.025 is a static value right now. This will be dynamic
         self.wh_taxes = self.amount_untaxed * self.TWH
-        # Calculate the sum of all taxes
-        self.amount_tax = sum(line.amount for line in self.tax_line_ids)
-        # Calculate the sum of total amount with all taxes
-        self.amount_total = self.amount_untaxed + self.amount_tax
-        amount_total_company_signed = self.amount_total
-        amount_untaxed_signed = self.amount_untaxed
-
-        if self.currency_id \
-                and self.currency_id != self.company_id.currency_id:
-            amount_total_company_signed = \
-                self.currency_id.compute(self.amount_total,
-                                         self.company_id.currency_id)
-            amount_untaxed_signed = self.currency_id.compute(
-                self.amount_untaxed, self.company_id.currency_id
-            )
-
-        sign = self.type in ['in_refund', 'out_refund'] and -1 or 1
-        self.amount_total_company_signed = amount_total_company_signed * sign
-        self.amount_total_signed = self.amount_total * sign
-        self.amount_untaxed_signed = amount_untaxed_signed * sign
         self.amount_total -= self.wh_taxes
+
