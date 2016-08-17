de799f490440297edfbf28280b7588e2c7e69947
Luis Alfredo da Silva <luis.adasilvaf@gmail.com>
2016-08-17 11:11:56 -0400

    aplicando al invoice los impuestos de la posición fiscal del cliente

--- models/l10n_co_tax_extension.py
+++ models/l10n_co_tax_extension.py
@@ -27,7 +27,7 @@ from openerp.tools.translate import _
 import logging
 _logger = logging.getLogger(__name__)
 
-class ColombianTaxes(models.Model):
+class AccountInvoice(models.Model):
     """ This Model calculates and saves withholding tax that apply in
     Colombia"""
 
@@ -51,14 +51,17 @@ class ColombianTaxes(models.Model):
         @return: void
         """
         # Calling the original calculation
-        super(ColombianTaxes, self)._compute_amount()
+        super(AccountInvoice, self)._compute_amount()
 
-        self.amount_without_wh_tax = self.amount_total
+        for tax in self.fiscal_position_id.tax_ids_invoice:
+            _logger.warn({ 'name': self.fiscal_position_id.name, 'tax': tax.tax_id.name })         
+
+        self.amount_tax = sum(line.amount for line in self.tax_line_ids if line.tax_id.name != 'WITHHOLDING')
+        self.amount_without_wh_tax = self.amount_untaxed + self.amount_tax
         # Extending the calculation with the colombian withholding tax
         # TODO: 0.025 is a static value right now. This will be dynamic
         self.wh_taxes = self.amount_untaxed * self.TWH
-        self.amount_total -= self.wh_taxes
-
+        # self.amount_total -= self.wh_taxes
         #Calling the original calculation did not call the local var sign.
         sign = self.type in ['in_refund', 'out_refund'] and -1 or 1
 
@@ -66,39 +69,39 @@ class ColombianTaxes(models.Model):
         # amount_total_signed again.
         self.amount_total_signed = self.amount_total * sign
 
-    @api.multi
-    def finalize_invoice_move_lines(self, move_lines):
-        account = self.env['account.account'].search([('code', '=', '135515')])
+    # @api.multi
+    # def finalize_invoice_move_lines(self, move_lines):
+    #     account = self.env['account.account'].search([('code', '=', '135515')])
 
-        for tp_line in move_lines:
-            line = tp_line[2]
-            if line['account_id'] == self.account_id.id:
-                line['debit'] = line['debit'] - self.wh_taxes
-                wh_line = (0, 0,
-                             {
-                                 'date_maturity': False,
-                                 'partner_id': self.partner_id.id,
-                                 'name': 'Retención a la fuente',
-                                 'debit': self.wh_taxes,
-                                 'credit': False,
-                                 'account_id': account.id,
-                                 'analytic_line_ids': False,
-                                 'amount_currency': 0,
-                                 'currency_id': False,
-                                 'quantity': 1,
-                                 'product_id': False,
-                                 'product_uom_id': False,
-                                 'analytic_account_id': False,
-                                 'invoice_id': self.id,
-                                 'tax_ids': False,
-                                 'tax_line_id': False,
-                             })        
+    #     for tp_line in move_lines:
+    #         line = tp_line[2]
+    #         if line['account_id'] == self.account_id.id:
+    #             line['debit'] = line['debit'] - self.wh_taxes
+    #             wh_line = (0, 0,
+    #                          {
+    #                              'date_maturity': False,
+    #                              'partner_id': self.partner_id.id,
+    #                              'name': 'Retención a la fuente',
+    #                              'debit': self.wh_taxes,
+    #                              'credit': False,
+    #                              'account_id': account.id,
+    #                              'analytic_line_ids': False,
+    #                              'amount_currency': 0,
+    #                              'currency_id': False,
+    #                              'quantity': 1,
+    #                              'product_id': False,
+    #                              'product_uom_id': False,
+    #                              'analytic_account_id': False,
+    #                              'invoice_id': self.id,
+    #                              'tax_ids': False,
+    #                              'tax_line_id': False,
+    #                          })        
         
-        move_lines.insert(-1, wh_line)
-        return move_lines
+    #     move_lines.insert(-1, wh_line)
+    #     return move_lines
 
     def _get_tax_amount_by_group(self):
-        res = super(ColombianTaxes, self)._get_tax_amount_by_group()
+        res = super(AccountInvoice, self)._get_tax_amount_by_group()
         groups_not_in_invoice = self.env['account.tax.group'].search([('not_in_invoice','=',True)])
 
         for g in groups_not_in_invoice:
@@ -117,7 +120,58 @@ class ColombianTaxes(models.Model):
                 res = True
 
         return res 
+    
+    @api.onchange('partner_id', 'company_id')
+    def _onchange_partner_id(self):
+        super(AccountInvoice, self)._onchange_partner_id()
 
+        account_invoice_tax = self.env['account.invoice.tax']
+        ctx = dict(self._context)
+        tax_grouped = {}
+        if self.fiscal_position_id:
+            fp = self.env['account.fiscal.position'].search([('id','=',self.fiscal_position_id.id)])
+            fp.ensure_one()
+            for base in fp.tax_ids_invoice:
+                tax = base.tax_id
+                val = {
+                        'invoice_id': self.id,
+                        'name': tax['name'],
+                        'tax_id': tax['id'],
+                        'amount': 0,
+                        'manual': False,
+                        'sequence': tax['sequence'],
+                        'account_analytic_id': tax['analytic'] or False,
+                        'account_id': self.type in ('out_invoice', 'in_invoice') and tax['account_id'].id or tax['refund_account_id'].id, 
+                    }
+
+                key = str(tax['id']) + '-' + str(val['account_id'])
+                
+                if key not in tax_grouped:
+                    tax_grouped[key] = val
+                else:
+                    tax_grouped[key]['amount'] += val['amount']
+        
+        for tax in tax_grouped.values():
+            _logger.warn(tax)
+            account_invoice_tax.create(tax)
+
+        # dummy write on self to trigger recomputations
+        self.with_context(ctx).write({'invoice_line_ids': []})
+        
+        tax_lines = self.tax_line_ids.browse([])
+        for tax in tax_grouped.values():
+            tax_lines += tax_lines.new(tax)
+        self.tax_line_ids = tax_lines
+
+
+class AccountInvoiceLine(models.Model):
+    _name = 'account.invoice.line'
+    _inherit = 'account.invoice.line'
+
+    def _set_taxes(self):
+        # _logger.warn({'name': fp.name, 'tax_ids': fp.tax_ids_invoice})
+        super(AccountInvoiceLine, self)._set_taxes()
+        
 class AccountTax(models.Model):
     _name = 'account.tax'
     _inherit = 'account.tax'
